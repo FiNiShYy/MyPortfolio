5fb6e0fe41723fcb8e39a7c52e466bc0248f562b
Kauã Gabriel <kauagabrielramkg@gmail.com>
2024-01-26 13:19:55 -0300

    update-test

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -17,27 +17,34 @@ const Portfolio = () => {
         <Cards
                 className='cards'
                 imageUrl={PortalSalt}
-                titulo='Portal Salt Systems'
-                link="https://todo-list-react-smoky-alpha.vercel.app/"
+                titulo='Teste Mariana Curiosa'
+                link="https://www.google.com/search?q=te+amo&sca_esv=601759512&rlz=1C1ONGR_pt-PTBR1073BR1073&tbm=isch&sxsrf=ACQVn0_VWMFmV5y9R9xMtg2HFdy5hnbYmw:1706286007489&source=lnms&sa=X&ved=2ahUKEwil5_HauvuDAxVTrpUCHcDFCTgQ_AUoAnoECAEQBA&biw=1920&bih=945&dpr=1"
         />
         <Cards
                 className='cards'
                 imageUrl={PortalSalt}
                 titulo='Portal Salt Systems'
-                link="https://todo-list-react-smoky-alpha.vercel.app/"
+                link="https://saltsystems.com.br/"
         />
         <Cards
                 className='cards'
                 imageUrl={PortalSalt}
                 titulo='Portal Salt Systems'
-                link="https://todo-list-react-smoky-alpha.vercel.app/"
+                link="https://saltsystems.com.br/"
         />
         <Cards
                 className='cards'
                 imageUrl={PortalSalt}
                 titulo='Portal Salt Systems'
-                link="https://todo-list-react-smoky-alpha.vercel.app/"
+                link="https://saltsystems.com.br/"
         />
+        <Cards
+                className='cards'
+                imageUrl={PortalSalt}
+                titulo='Portal Salt Systems'
+                link="https://saltsystems.com.br/"
+        />
+        
         </div>
         </div>
         )
