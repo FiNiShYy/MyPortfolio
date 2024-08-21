530a300b924dc856e34d14d3e089378b334c379b
FiNiShYy <kauagabrielramkg@gmail.com>
2024-08-21 10:35:52 -0300

    update text about me

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <img src={PFPKaua}></img>
             <div className="about-text">
                 <h1>SOBRE MIM</h1>
-                <h2>Sou desenvolvedor iniciante em busca de desafios, atualmente meu foco está voltado para Front End, nessa área possuo conhecimentos acerca de HTML, CSS, Figma e estou trabalhando em alguns projetos React, também tenho vontade de aprender mais sobre Back End onde ja estudei sobre Node.js</h2>
+                <h2>Sou desenvolvedor em busca de constantes desafios, atualmente meu foco está voltado para Front End, possuo conhecimentos e experiências em trabalhos reais com as tecnologías HTML, CSS, Javascript, React, Figma e bibliotecas diversas. Também tenho conhecimento acerca de Back End com conhecimentos sobre Node e logs.</h2>
             </div>
         </div>
     )
