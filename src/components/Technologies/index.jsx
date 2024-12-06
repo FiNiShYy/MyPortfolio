e5e348405f3abc03e9542ecf8127d608e4150b4c
FiNiShYy <kauagabrielramkg@gmail.com>
2024-12-06 19:24:34 -0300

    update technologies href

--- src/components/Technologies/index.jsx
+++ src/components/Technologies/index.jsx
@@ -6,37 +6,37 @@ const Technologies = ({ isEnglish }) => {
             <h1>{isEnglish ? "TECHNOLOGIES" : "TECNOLOGIAS"}</h1>
             <ul>
                 <li className='column'>
-                    <a href="#">
+                    <a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" target="_blank">
                         <i className="fab fa-html5"></i>
                         <span>HTML5</span>
                     </a>
                 </li>
                 <li className='column'>
-                    <a href="#">
+                    <a href="https://www.w3schools.com/cssref/index.php" target="_blank">
                         <i className="fab fa-css3"></i>
                         <span>CSS3</span>
                     </a>
                 </li>
                 <li className='column'>
-                    <a href="#">
+                    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
                         <i className="fab fa-js"></i>
                         <span>JavaScript</span>
                     </a>
                 </li>
                 <li className='column'>
-                    <a href="#">
+                    <a href="https://react.dev" target="_blank">
                         <i className="fab fa-react"></i>
                         <span>React.Js</span>
                     </a>
                 </li>
                 <li className='column'>
-                    <a href="#">
+                    <a href="https://www.figma.com" target="_blank">
                         <i className="fab fa-figma"></i>
                         <span>Figma</span>
                     </a>
                 </li>
                 <li className='column'>
-                    <a href="#">
+                    <a href="https://nodejs.org/en" target="_blank">
                         <i className="fab fa-node"></i>
                         <span>Node</span>
                     </a>
